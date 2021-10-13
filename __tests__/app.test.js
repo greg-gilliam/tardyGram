2dc209931a31a58e781b1ba728243dc4ff4e1d4f
satyadvaya <satyadvaya@gmail.com>
2021-10-13 07:03:49 -0700

    hailmary

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,55 @@ const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
 
-describe('tardyGram routes', () => {
+jest.mock('../lib/middleware/ensureAuth.js', () => {
+  return (req, res, next) => {
+    req.user = {
+      username: 'test_user',
+      photoUrl: 'https://example.com/image.png',
+      iat: Date.now(),
+      exp: Date.now(),
+    };
+
+    next();
+  };
+});
+
+// function () {
+//   return function (req, res, next) {
+//     req.user = ...
+//   }
+// }
+
+// jest.mock('../lib/data/users.js', () => {
+//   return [{ user_id: 4 }, { user_id: 5 }, { user_id: 6 }];
+// });
+
+describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('tests something', async () => {});
-
   afterAll(() => {
     pool.end();
   });
+
+  it('returns a user object from GET /api/auth/me when logged in', async () => {
+    const res = await request(app).get('/api/auth/me');
+
+    expect(res.body).toEqual({
+      username: 'test_user',
+      photoUrl: 'https://example.com/image.png',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+
+  // it('requires a logged in user to GET /api/auth/me', async () => {
+  //   const res = await request(app).get('/api/auth/me');
+
+  //   expect(res.body).toEqual({
+  //     status: 401,
+  //     message: 'You must be signed in to continue.',
+  //   });
+  // });
 });
